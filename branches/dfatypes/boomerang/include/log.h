c0ecb4d1c43b37f3ebb486b3405186b66dacdeed
nobody <nobody@dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5>
2004-09-24 06:11:42 +0000

    This commit was manufactured by cvs2svn to create branch 'dfatypes'.

---
+++ branches/dfatypes/boomerang/include/log.h
@@ -0,0 +1,39 @@
+
+#ifndef LOG_H
+#define LOG_H
+
+#include "types.h"
+#include <fstream>
+
+class Statement;
+class Exp;
+class LocationSet;
+
+class Log 
+{
+public:
+	Log() { }
+	virtual Log &operator<<(const char *str) = 0;
+	virtual Log &operator<<(Statement *s);
+	virtual Log &operator<<(Exp *e);
+	virtual Log &operator<<(int i);
+	virtual Log &operator<<(char c);
+	virtual Log &operator<<(double d);
+	virtual Log &operator<<(ADDRESS a);
+	virtual Log &operator<<(LocationSet *l);
+	virtual ~Log() {};
+};
+
+class FileLogger : public Log {
+public:
+	FileLogger();		// Implemented in boomerang.cpp
+	virtual Log &operator<<(const char *str) { 
+		out << str << std::flush;  
+		return *this; 
+	}
+	virtual ~FileLogger() {};
+protected:
+	std::ofstream out;
+};
+
+#endif
