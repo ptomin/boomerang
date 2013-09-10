1a3d51308c51215e95704afdcf7f67570c0957c5
vwoodzell <vwoodzell@dc5e1367-85d4-4bd1-96b4-a20bfbfd92d5>
2013-09-10 01:24:10 +0000

    Remove merged branches.

--- branches/summdf/boomerang/signatures/common.hs
+++
@@ -1,6 +0,0 @@
-stdio.h
-gcc3.h
-gcc2.9.h
-math.h
-stdlib.h
-string.h
--- branches/withtypes/boomerang/signatures/unistd.h
+++
@@ -1,4 +0,0 @@
-int getopt(int argc, char *argv[], const char *optstring);
-size_t write(int fd, const void *buf, size_t count);
-size_t read(int fd, void *buf, size_t count);
-
